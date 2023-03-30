faf180cc75be61b926c191564d8fbc617c32a9a5
jcromerohdz <jcromerohdz@gmail.com>
2023-03-30 15:34:46 -0700

    Setup VueJS 2023 way

---
+++ studynet_django/studynet_vue/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+
+import 'bulma/css/bulma.css'
+
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router, axios)
+
+app.mount('#app')
