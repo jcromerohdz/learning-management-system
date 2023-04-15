b4c404d37e076136e121661f8d719322c9386521
jcromerohdz <jcromerohdz@gmail.com>
2023-04-15 11:07:59 -0700

    Hide login/sign up buttons in the menu bar when is authenticated, also add a new menu item for courses

--- studynet_django/studynet_vue/src/components/NavBar.vue
+++ studynet_django/studynet_vue/src/components/NavBar.vue
@@ -1,4 +1,12 @@
-<script></script>
+<script setup>
+  import { storeToRefs } from 'pinia';
+  import { useUserAuth } from '@/stores/userAuth'
+
+  const userAuth = useUserAuth()
+
+  const { user } = storeToRefs(userAuth)
+
+</script>
 <template>
   <nav
     class="navbar is-dark"
@@ -14,17 +22,24 @@
       <div class="navbar-start">
         <RouterLink to="/" class="navbar-item">Home</RouterLink>
         <RouterLink to="/about" class="navbar-item">About</RouterLink>
+        <RouterLink to="/courses" class="navbar-item">Courses</RouterLink>
       </div>
 
       <div class="navbar-end">
         <div class="navbar-item">
           <div class="buttons">
-            <RouterLink to="/signup" class="button is-primary">
-              <strong>Sign Up</strong>
-            </RouterLink>
-            <RouterLink to="/login" class="button is-light">
-              <strong>Log In</strong>
-            </RouterLink>
+            
+            <template v-if="user.isAuthenticated">
+              <RouterLink to="/dashboard/my-account" class="button is-info">My Account</RouterLink>
+            </template>
+            <template v-else>
+              <RouterLink to="/signup" class="button is-primary">
+                <strong>Sign Up</strong>
+              </RouterLink>
+              <RouterLink to="/login" class="button is-light">
+                <strong>Log In</strong>
+              </RouterLink>
+            </template>
           </div>
         </div>
       </div>
