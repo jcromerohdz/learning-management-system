f4906c10c8b250fe7026687886397ef1204d6d7a
jcromerohdz <jcromerohdz@gmail.com>
2023-04-24 11:34:43 -0700

    Convert the course list object into a component

---
+++ studynet_django/studynet_vue/src/components/CourseItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+  defineProps(['course'])
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="https://placehold.co/1280x960" alt="Placeholder Image">
+      </figure>
+    </div>
+
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="is-sizes-5">{{ course.title }}</p>
+        </div>
+      </div>
+
+      <div class="content">
+        <p>{{ course.short_description }}</p>
+
+        <router-link :to="{name: 'course', params: {slug: course.slug}}">More Info...</router-link>
+      </div>
+    </div>
+  </div>
+</template>
--- studynet_django/studynet_vue/src/views/CoursesView.vue
+++ studynet_django/studynet_vue/src/views/CoursesView.vue
@@ -3,6 +3,8 @@
   import { ref } from 'vue';
   import axios from 'axios'
 
+  import CourseItem from '@/components/CourseItem.vue'
+
   const courses = ref([])
 
   const getCourses = async () => {
@@ -50,27 +52,11 @@
                 v-for="course in courses"
                 :key="course.id"
                 >
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://placehold.co/1280x960" alt="Placeholder Image">
-                    </figure>
-                  </div>
+                
+                <CourseItem 
+                  :course="course"
+                />
 
-                  <div class="card-content">
-                    <div class="media">
-                      <div class="media-content">
-                        <p class="is-sizes-5">{{ course.title }}</p>
-                      </div>
-                    </div>
-
-                    <div class="content">
-                      <p>{{ course.short_description }}</p>
-
-                      <router-link :to="{name: 'course', params: {slug: course.slug}}">More Info...</router-link>
-                    </div>
-                  </div>
-                </div>
               </div>
  
             </div>
