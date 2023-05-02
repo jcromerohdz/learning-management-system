ca192de6d8fc155bf368bc07553379b0d03081fe
jcromerohdz <jcromerohdz@gmail.com>
2023-05-01 18:36:00 -0700

    Make a new serializer for Quiz model

--- studynet_django/course/models.py
+++ studynet_django/course/models.py
@@ -79,7 +79,10 @@ class Quiz(models.Model):
     answer = models.CharField(max_length=200, null=True)
     op1 = models.CharField(max_length=200, null=True)
     op2 = models.CharField(max_length=200, null=True)
-    op2 = models.CharField(max_length=200, null=True)
+    op3 = models.CharField(max_length=200, null=True)
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
 
     def __str__(self):
         return self.question
--- studynet_django/course/serializers.py
+++ studynet_django/course/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Course, Lesson, Comment, Category
+from .models import Course, Lesson, Comment, Category, Quiz
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -26,3 +26,8 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'name', 'content', 'created_at']
+
+class QuizSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quiz
+        fields = ['id', 'lesson_id', 'question', 'answer', 'op1', 'op2', 'op3']
