7054bd196117f1f8081203f93970a46ab18d9a84
jcromerohdz <jcromerohdz@gmail.com>
2023-05-08 09:50:47 -0700

    Show active courses in my account

--- studynet_django/studynet_vue/src/views/dashboard/MyAccountView.vue
+++ studynet_django/studynet_vue/src/views/dashboard/MyAccountView.vue
@@ -1,13 +1,18 @@
 <script setup>
   import axios from 'axios'
+  import { ref } from 'vue'
   import { useRoute, useRouter } from "vue-router"
   import { useUserAuth } from '@/stores/userAuth'
+
+  import CourseItem from '../../components/CourseItem.vue'
 
   const useAuth = useUserAuth()
   const { removeToken } = useAuth
   const router = useRouter();
 
-    document.title = 'My Account | StudyNet'
+  const courses = ref([])
+
+  document.title = 'My Account | StudyNet'
 
   const logout = async () => {
     try {
@@ -29,6 +34,7 @@
     try {
       const response = await axios.get('/api/v1/activities/get_active_courses')
       console.log(response.data)
+      courses.value = response.data
     } catch (error) {
       console.log(error)
     }
@@ -49,6 +55,26 @@
     </div>
 
     <section class="section">
+      <div class="columns is-multiline">
+        <div class="column is-12">
+          <h2 class="subtitle is-size-3">
+            Your active courses
+          </h2>
+        </div>
+        <div 
+          class="column is-4"
+          v-for="course in courses"
+          :key="course.id"
+        >
+          <CourseItem 
+            :course="course"
+          />
+        </div>
+
+      </div>
+
+      <hr>
+
       <button @click="logout()" class="button is-danger">Log out</button>
     </section>
   </div>
