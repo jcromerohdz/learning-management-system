6a9888f5a5f6a444eac17920d0e4593348ddabed
jcromerohdz <jcromerohdz@gmail.com>
2023-05-13 21:14:12 -0700

    Adding status field to the Course model

--- studynet_django/course/models.py
+++ studynet_django/course/models.py
@@ -16,6 +16,16 @@ class Category(models.Model):
 
 
 class Course(models.Model):
+    DRAFT = 'draft'
+    IN_REVIEW = 'in_review'
+    PUBLISHED = 'published'
+
+    STATUS_CHOICES = (
+        (DRAFT, 'Draft'),
+        (IN_REVIEW, 'In Review'),
+        (PUBLISHED, 'Published'),
+    )
+
     categories = models.ManyToManyField(Category)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -24,6 +34,7 @@ class Course(models.Model):
     created_at = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='courses', on_delete = models.CASCADE)
     image = models.ImageField(upload_to='uploads', blank=True, null=True)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=DRAFT)
 
     class Meta:
         ordering = ('-created_at',)
